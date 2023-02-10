3853b37a146c76ef5d7cc076ea5a22b7abc4cf52
SeaBee <517032401@qq.com>
2023-02-10 17:58:41 +0800

    进行一个胡乱的适配

--- static/js/menu.js
+++ static/js/menu.js
@@ -12,3 +12,14 @@ t_menus.forEach(item => item.addEventListener('click', () => {
     console.log(`t_${item.id}`);
     document.getElementById(`t_${item.id}`).scrollIntoView(true);
 }));
+
+const side_menu_btn = document.querySelector(".menu .btn");
+const side_menu = document.querySelector(".menu");
+side_menu_btn.addEventListener('click', () => {
+    console.log("hello")
+    if(side_menu.classList.contains("taylor")) {
+        side_menu.classList.remove("taylor");
+    } else {
+        side_menu.classList.add("taylor");
+    }
+})
